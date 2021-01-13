a7377347d8353e9c807d0bb9ab7f6ae1ce07e45c
Ruslan_Yakovlev <r.yakovlev@takeoff-staff.ru>
2021-01-13 15:30:07 +0500

    complete step

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'angular-task';
+export class AppComponent implements OnInit {
+
+  public form: FormGroup;
+
+  /** инициализация */
+  ngOnInit(): void {
+    this.form = new FormGroup({
+      control: new FormControl('')
+    })
+  }
+
+  /** сабмит формы */
+  public submit(): void {
+    console.log(this.form.value);
+    this.form.reset();
+  }
+
 }
